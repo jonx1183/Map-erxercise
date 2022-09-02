6f264bdd46425c4dad9dbc8c906c475396290c78
jonx1183 <jonx1183@stud.kea.dk>
2022-09-02 11:45:33 +0200

    Storbælts begyndt

---
+++ src/StorbæltsBroen/Test.java
@@ -0,0 +1,20 @@
+package StorbæltsBroen;
+
+public class Test {
+  public static void main(String[] args) {
+    Test main = new Test();
+    main.go();
+  }
+
+  public void go(){
+
+  }
+
+  //A method that creates a HashMap, that can store the information on the cars.
+  //A hashMap would be more convinent for finding information on a specific car.
+
+  //There would need to be a way to teel if a new car passes, maybe with a for loop?
+
+  //For the second part:
+  //A method that determins the size of the HashMap for each bridge, and then add the sizes together.
+}
